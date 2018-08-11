3a27483e3def78fded4104df992e7c72b089ba2e
Tran Thang <thangtran.se@gmail.com>
2018-08-11 21:09:37 +0700

    get "/"

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
+app.get('/', (req, res) => {  
+	req.end("oke");
+});
+
 // Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {  
  
