de868adb374bb7a0379303d45140d396b5109f09
Tran Thang <thangtran.se@gmail.com>
2018-08-12 16:29:51 +0700

    Update send/receive

--- server.js
+++ server.js
@@ -1,58 +1,102 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express().use(bodyParser.json()); 
+const app = express().use(bodyParser.json());
 
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
-app.get('/', (req, res) => {  
-	req.end("oke");
+app.get('/', (req, res) => {
+    req.end("oke");
 });
+
 
 // Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {
-	let body = req.body;
-	console.log("body", body.entry[0].changes);
-	// Checks this is an event from a page subscription
-	if (body.object === 'page') {
-		// Iterates over each entry - there may be multiple if batched
-		body.entry.forEach(function(entry) {
-		  // Gets the message. entry.messaging is an array, but 
-		  // will only ever contain one message, so we get index 0
-		  let webhook_event = entry.messaging[0];
-		  console.log("xuất hiện: ",webhook_event);
-		});
-		// Returns a '200 OK' response to all requests
-		res.status(200).send('EVENT_RECEIVED');
-	} else {
-		// Returns a '404 Not Found' if event is not from a page subscription
-		res.sendStatus(404);
-	}
+    let body = req.body;
+    console.log("body", body.entry[0].changes);
+    // Checks this is an event from a page subscription
+    if (body.object === 'page') {
+        // Iterates over each entry - there may be multiple if batched
+        body.entry.forEach(function (entry) {
+            // Gets the message. entry.messaging is an array, but
+            // will only ever contain one message, so we get index 0
+            let webhook_event = entry.messaging[0];
+            console.log("xuất hiện: ", webhook_event);
+
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
+
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);
+            } else if (webhook_event.postback) {
+                handlePostback(sender_psid, webhook_event.postback);
+            }
+
+        });
+        // Returns a '200 OK' response to all requests
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
+        // Returns a '404 Not Found' if event is not from a page subscription
+        res.sendStatus(404);
+    }
 
 });
 
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
 
-  // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "thang_dep_trai"
-    
-  // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
-    
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
-	console.log("call this");
-	
-    // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);      
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN = "thang_dep_trai"
+
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+        console.log("call this");
+
+        // Checks the mode and token sent is correct
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+            // Responds with the challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
     }
-  }
 });
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+    let response;
+
+    // Check if the message contains text
+    if (received_message.text) {
+
+        // Create the payload for a basic text message
+        response = {
+            "text": `You sent the message: "${received_message.text}". Now send me an image!`
+        }
+    }
+
+    // Sends the response message
+    callSendAPI(sender_psid, response);
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+// Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    }
+}
