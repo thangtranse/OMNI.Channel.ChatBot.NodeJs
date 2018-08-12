b267997bc6c73f1aedbd4574bcd03ffd7330a5be
Tran Thang <thangtran.se@gmail.com>
2018-08-12 16:58:04 +0700

    update msg

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ function handleMessage(sender_psid, received_message) {
 
     // Sends the response message
     // callSendAPI(sender_psid, response);
-    sendMsgToRocket(response)
+    sendMsgToRocket(received_message.text)
 }
 
 // Handles messaging_postbacks events
