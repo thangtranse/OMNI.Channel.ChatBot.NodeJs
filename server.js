bb2c58434971a71fca347ea86fcccc9f5f4df4ed
Tran Thang <thangtran.se@gmail.com>
2018-08-14 14:47:25 +0700

    error

--- server.js
+++ server.js
@@ -44,19 +44,20 @@ passport.use(new FacebookStrategy(configAuth.facebookAuth,
         process.nextTick(function () {
             console.log("accessToken", accessToken);
             api.loginWithFacebook(accessToken);
-            window.close();
         });
     }
 ));
+
 // END FB
 
-// app.get('/', (req, res) => {
-//     res.writeHead(200, {'Content-Type': 'text/html'});
-//     fs.readFile('index.html', (err, data) => {
-//         if (err) throw err;
-//         res.end(data);
-//     });
-// });
+app.get('/', (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('index.html', (err, data) => {
+        if (err) throw err;
+        res.end(data);
+    });
+
+});
 
 
 // Creates the endpoint for our webhook
