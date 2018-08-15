5c094ce254142c7866719b68854e7bd51149a949
Tran Thang <thangtran.se@gmail.com>
2018-08-15 16:17:04 +0700

    lên

--- server.js
+++ server.js
@@ -169,7 +169,7 @@ var handleMessage = (sender_psid, received_message) => {
                         "text": received_message
                     }
             }
-            api.sendMess('wmFKvXKW3e34hpyzZ', received_message.text, data.token_rocket.stringValue, data.id_rocket.stringValue,
+            api.sendMess('zWE5XTd4td6pxMifr', received_message.text, data.token_rocket.stringValue, data.id_rocket.stringValue,
                 data => {
                     console.log("tin nhắn được gửi đến rocket: ", data);
                 });
