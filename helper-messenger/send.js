922d3cf99512986bc2a62a4fc6d482ba7fda1992
Tran Thang <thangtran.se@gmail.com>
2018-08-16 17:48:12 +0700

    về thôi

--- helper-messenger/send.js
+++ helper-messenger/send.js
@@ -38,6 +38,44 @@ const sendMessengerTemplateList = (sender_psid, list) => {
                                 "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
                             }
                         ]
+                    },
+                    {
+                        "title": "Classic White T-Shirt",
+                        "subtitle": "See all our colors",
+                        "default_action": {
+                            "type": "web_url",
+                            "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
+                            "messenger_extensions": false,
+                            "webview_height_ratio": "tall"
+                        }
+                    }, {
+                        "title": "Classic Blue T-Shirt",
+                        "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+                        "subtitle": "100% Cotton, 200% Comfortable",
+                        "default_action": {
+                            "type": "web_url",
+                            "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
+                            "messenger_extensions": true,
+                            "webview_height_ratio": "tall",
+                            "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                        },
+                        "buttons": [
+                            {
+                                "title": "Shop Now",
+                                "type": "web_url",
+                                "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+                                "messenger_extensions": true,
+                                "webview_height_ratio": "tall",
+                                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                            }
+                        ]
+                    }
+                ],
+                "buttons": [
+                    {
+                        "title": "View More",
+                        "type": "postback",
+                        "payload": "payload"
                     }
                 ]
             }
