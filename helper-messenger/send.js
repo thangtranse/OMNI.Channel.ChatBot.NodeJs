d59d43a0fae6f636a414a8b55a9bf521b1d99980
Tran Thang <thangtran.se@gmail.com>
2018-08-17 09:08:04 +0700

    1122

--- helper-messenger/send.js
+++ helper-messenger/send.js
@@ -33,7 +33,9 @@ const sendMessengerTemplateList = (sender_psid, list) => {
                 "payload": {
                     "template_type": "list",
                     "top_element_style": "compact",
-                    "elements": temp,
+                    "elements": [
+                        temp
+                    ],
                 }
             }
         }
@@ -44,6 +46,7 @@ const sendMessengerTemplateList = (sender_psid, list) => {
     }
     console.log("----------------------------ththththt----------------------------");
     console.log(test);
+    console.log(test.attachment.payload.elements);
     console.log("----------------------------ththththt----------------------------");
     parameterSentGraph("messages?access_token=" + PAGE_ACCESS_TOKEN, sender_psid, test);
 }
