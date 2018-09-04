5997e780dfe29167c4d443cae8dc3ad7b846f9ad
Tran Thang <thangtran.se@gmail.com>
2018-09-04 09:19:40 +0700

    fix err

--- helper-zalo/apiOpen.js
+++ helper-zalo/apiOpen.js
@@ -1,12 +1,15 @@
-const ZaloSocial = require('zalo-sdk').ZaloSocial;
-const config = require('../config');
+// const ZaloSocial = require('zalo-sdk').ZaloSocial;
 // const zsConfig = config.zalo;
 // const ZSClient = new ZaloSocial(zsConfig);
+
+const config = require('../config');
+var ZaloOA = require('zalo-sdk').ZaloOA;
 const code = config.zalotoken;
 var zaConfig = {
     oaid: config.zalo.appId,
     secretkey: config.zalo.secretkey
 }
+
 const sending = (_userId, _message) => {
     var ZOAClient = new ZaloOA(zaConfig);
     return new Promise((resolve, reject) => {
