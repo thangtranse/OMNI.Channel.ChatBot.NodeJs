5fec79459f1805c86716cb8fef56e1005a5033c4
Tran Thang <thangtran.se@gmail.com>
2018-09-04 09:10:46 +0700

    fix error config

--- helper-zalo/apiOpen.js
+++ helper-zalo/apiOpen.js
@@ -5,12 +5,6 @@ const ZSClient = new ZaloSocial(zsConfig);
 const code = config.zalotoken;
 
 const sending = (_userId, _message) => {
-    console.log("tmt: ", code);
-    ZSClient.getAccessTokenByOauthCode(code, function (response) {
-        if (response && response.access_token) {
-            ZSClient.setAccessToken(response.access_token);
-        }
-    });
     return new Promise((resolve, reject) => {
         ZOAClient.api('sendmessage/text', 'POST', {uid: _userId, message: _message}, (response) => {
             resolve(response);
