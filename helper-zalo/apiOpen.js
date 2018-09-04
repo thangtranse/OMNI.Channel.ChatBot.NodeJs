fbab8fa0de21d3678619ef32677e7c5e11ebcffc
Tran Thang <thangtran.se@gmail.com>
2018-09-04 09:08:10 +0700

    Fix error conffig

--- helper-zalo/apiOpen.js
+++ helper-zalo/apiOpen.js
@@ -1,19 +1,16 @@
-var ZaloSocial = require('zalo-sdk').ZaloSocial;
-var config = require('../config');
-
-var zsConfig = config.zalo;
-
-var ZSClient = new ZaloSocial(zsConfig);
-var code = config.zalotoken;
-
-ZSClient.getAccessTokenByOauthCode(code, function (response) {
-    if (response && response.access_token) {
-        ZSClient.setAccessToken(response.access_token);
-    }
-});
-
+const ZaloSocial = require('zalo-sdk').ZaloSocial;
+const config = require('../config');
+const zsConfig = config.zalo;
+const ZSClient = new ZaloSocial(zsConfig);
+const code = config.zalotoken;
 
 const sending = (_userId, _message) => {
+    console.log("tmt: ", code);
+    ZSClient.getAccessTokenByOauthCode(code, function (response) {
+        if (response && response.access_token) {
+            ZSClient.setAccessToken(response.access_token);
+        }
+    });
     return new Promise((resolve, reject) => {
         ZOAClient.api('sendmessage/text', 'POST', {uid: _userId, message: _message}, (response) => {
             resolve(response);
