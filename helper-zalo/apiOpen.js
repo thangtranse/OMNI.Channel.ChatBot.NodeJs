41fdb6774924240ffe62c8d9bbb38df60cb7c1da
Tran Thang <thangtran.se@gmail.com>
2018-09-04 09:15:07 +0700

    fix conffig

--- helper-zalo/apiOpen.js
+++ helper-zalo/apiOpen.js
@@ -1,10 +1,14 @@
 const ZaloSocial = require('zalo-sdk').ZaloSocial;
 const config = require('../config');
-const zsConfig = config.zalo;
-const ZSClient = new ZaloSocial(zsConfig);
+// const zsConfig = config.zalo;
+// const ZSClient = new ZaloSocial(zsConfig);
 const code = config.zalotoken;
-
+var zaConfig = {
+    oaid: config.zalo.appId,
+    secretkey: config.zalo.secretkey
+}
 const sending = (_userId, _message) => {
+    var ZOAClient = new ZaloOA(zaConfig);
     return new Promise((resolve, reject) => {
         ZOAClient.api('sendmessage/text', 'POST', {uid: _userId, message: _message}, (response) => {
             resolve(response);
