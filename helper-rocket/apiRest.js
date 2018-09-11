a9e7aab718074620d74128d60fda1c5536f4f055
Tran Thang <thangtran.se@gmail.com>
2018-09-11 17:08:58 +0700

    fix Unikey

--- helper-rocket/apiRest.js
+++ helper-rocket/apiRest.js
@@ -172,6 +172,7 @@ class apiRest {
      * }
      */
     createChannel(channelName, _useridAdmin, _authAdmin, callback) {
+        channelName  = clearUnikey(channelName);
         axiosInstance({
             method: 'POST',
             url: 'channels.create',
@@ -231,6 +232,24 @@ const randomUsername = _email => (`${_email.replace(/(\s)/g, ".")}.${Math.floor(
  */
 const error = err => {
     console.log("API ERR: ", err.response);
+}
+
+const clearUnikey = _data => {
+    _data = _data.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+    _data = _data.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+    _data = _data.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+    _data = _data.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+    _data = _data.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+    _data = _data.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+    _data = _data.replace(/đ/g, "d");
+    _data = _data.replace(/À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ/g, "A");
+    _data = _data.replace(/È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ/g, "E");
+    _data = _data.replace(/Ì|Í|Ị|Ỉ|Ĩ/g, "I");
+    _data = _data.replace(/Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ/g, "O");
+    _data = _data.replace(/Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ/g, "U");
+    _data = _data.replace(/Ỳ|Ý|Ỵ|Ỷ|Ỹ/g, "Y");
+    _data = _data.replace(/Đ/g, "D");
+    return _data;
 }
 
 module.exports = new apiRest();
