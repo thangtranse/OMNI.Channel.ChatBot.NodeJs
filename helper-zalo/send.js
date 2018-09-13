c874e651718daa9b64a014539459b37c1b2b147f
Tran Thang <thangtran.se@gmail.com>
2018-09-13 10:17:51 +0700

    13123

--- helper-zalo/send.js
+++ helper-zalo/send.js
@@ -1,9 +1,12 @@
 const ProcessStr = require('../libs/processStr');
+const apiOpen = require('./apiOpen');
 
 const forwardZalo = (_data) => {
     console.log("thangtm: ", _data);
-    console.log("thangtm: ", _data.channel_name);
 
+    let uidZalo = ProcessStr.getIdFormRocket(_data.channel_name);
+    console.log("thangtm: ", uidZalo);
+    apiOpen.sending(uidZalo, _data.text);
 }
 
 
