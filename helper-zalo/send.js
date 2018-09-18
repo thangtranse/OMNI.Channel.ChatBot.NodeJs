9a8174a8078a5a10cca3b81f1624f3aaf35ea020
Tran Thang <thangtran.se@gmail.com>
2018-09-18 14:10:07 +0700

    public

--- helper-zalo/send.js
+++ helper-zalo/send.js
@@ -8,21 +8,12 @@ const forwardZalo = async (_data) => {
     /**
      * _data: {bot, channel_id, channel_name, message_id, timestamp, user_id, user_name, text, alias}
      */
-
-    let msgRocket = msgRocketModel.find({"idRoomRocket": _data.channel_id}, (err, result) => {
-        return new Promise((resolve, reject) => {
-            if (err) reject(err)
-            else resolve(result)
-        })
-    })
-
-    var getDataUser = await msgRocket.then(data => data)
+    var getDataUser = await mongodb.findOne(config.mongodb.collection, {"idRoomRocket": _data.channel_id}).then(data => data);
 
     if (getDataUser && _data.user_name.trim() != config.rocket.username) {
-        let uidZalo = getDataUser.uid;
+        let uidZalo = getDataUser.userId;
         apiOpen.sending(uidZalo, _data.text);
     }
 }
-
 
 module.exports = {forwardZalo}
