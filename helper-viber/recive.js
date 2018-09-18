b68760aa812fe5ff3b6d367165f39f4d4931aa74
Tran Thang <thangtran.se@gmail.com>
2018-09-18 14:44:54 +0700

    up

--- helper-viber/recive.js
+++ helper-viber/recive.js
@@ -1,6 +1,8 @@
 const apiRocket = require("../helper-rocket/apiRest");
 const ProcessStr = require("../libs/processStr");
 const mongodb = require("../database/mongodb");
+const msgRocketModel = require("../libs/models/msgRocket")
+const forwardRocket = require("../libs/forwardRocket")
 const config = require("../config");
 
 const handleMessage = async (_data) => {
@@ -12,12 +14,8 @@ const handleMessage = async (_data) => {
      * Chưa xử lý trường hợp seen, delivery
      */
     if (_data.event != 'message') return;
-
     var checkDataUser = await mongodb.findOne(config.mongodb.collection, {"uid": _data.sender.id}).then(data => data);
-
-    let inforUser = {};
     var idRoomRocket = null;
-    console.log("checkDataUser: ", checkDataUser);
     if (checkDataUser) {
         idRoomRocket = checkDataUser.idRoomRocket;
     } else { // chưa có nè
@@ -25,7 +23,6 @@ const handleMessage = async (_data) => {
         nameSender = ProcessStr.clearUnikey(nameSender);
         let nameRoomRocket = `Viber.${nameSender}`;
         let infoRoomRocket = await apiRocket.infoChannel(nameRoomRocket).then(data => data).catch(data => data);
-
         if (infoRoomRocket.success) {
             idRoomRocket = infoRoomRocket.channel._id;
         } else {
@@ -33,33 +30,14 @@ const handleMessage = async (_data) => {
             // Phương thức không đồng bộ
             let createWebhookRocket = apiRocket.createOutGoingWebhookRocket_VIBER(nameRoomRocket).then(data => data).catch(data => data);
             idRoomRocket = createRoomRocket.success ? createRoomRocket.channel._id : undefined;
-
         }
 
-        inforUser.infor = _data.sender;
-        inforUser.localSent = "Viber";
-        inforUser.nameRoomRocket = nameRoomRocket;
-        inforUser.idRoomRocket = idRoomRocket;
-        inforUser.uid = _data.sender.id;
-
-
-        var insertDataUser = await mongodb.insert(config.mongodb.collection, inforUser).then(data => data);
+        let msgRocket = new msgRocketModel.msgRocket("Viber", idRoomRocket, nameRoomRocket, _data, sender.id, _data.sender);
+        mongodb.insert(config.mongodb.collection, msgRocket.toJson()).then(data => data);
     }
     // kiểm tra giá trị
     if (typeof idRoomRocket == "undefined") return;
-
-    forwardRocket(idRoomRocket, _data);
-}
-
-// Chuyển tiếp tin nhắn ZALO sang Rocket
-const forwardRocket = (_idRoomRocket, _dataMsg) => {
-    console.log("_idRoomRocket", _idRoomRocket);
-    console.log("_dataMsg.message", _dataMsg.message);
-    console.log(" _dataMsg.sender.name", _dataMsg.sender.name);
-    console.log("_dataMsg.sender.avatar", _dataMsg.sender.avatar);
-
-    apiRocket.sendMsgRock(_idRoomRocket,
-        _dataMsg.message.text, _dataMsg.sender.name, _dataMsg.sender.avatar);
+    forwardRocket.forwardRocket(idRoomRocket, _data.message.text, _data.sender.name, _data.sender.avatar)
 }
 
 module.exports = {handleMessage}
--- helper-viber/send.js
+++ helper-viber/send.js
@@ -3,7 +3,6 @@ const apiViber = require("./apiViber");
 const config = require("../config");
 
 const forwardViber = async (_data) => {
-    console.log("forwardViber: ", _data);
     var getDataUser = await mongodb.findOne(config.mongodb.collection, {"idRoomRocket": _data.channel_id}).then(data => data);
     if (getDataUser && _data.user_name.trim() != config.rocket.username) {
         let uidViber = getDataUser.uid;
