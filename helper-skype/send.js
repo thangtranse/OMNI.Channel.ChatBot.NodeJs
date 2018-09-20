61bf1b2d075884210d36d8ab408320849a126e7b
Tran Thang <thangtran.se@gmail.com>
2018-09-20 15:30:39 +0700

    up

--- helper-skype/send.js
+++ helper-skype/send.js
@@ -7,7 +7,7 @@ const forwardSkype = async (_data) => {
     var getDataUser = await mongodb.findOne(config.mongodb.collection, {"idRoomRocket": _data.channel_id}).then(data => data);
     if (getDataUser && _data.user_name.trim() != config.rocket.username) {
         getDataUser.userDetail.text = _data.text;
-        apiSkype.sendMsg(getDataUser.userDetail.serviceUrl, getDataUser.userDetail.conversation.id, getDataUser.userDetail.id, getDataUser.userDetail);
+        await apiSkype.sendMsg(getDataUser.userDetail.serviceUrl, getDataUser.userDetail.conversation.id, getDataUser.userDetail.id, getDataUser.userDetail);
     }
 }
 
