2ad18e3c322c2ee7a037ee5f9b5e8e8c9a19a281
thangse <thangtran.se@gmail.com>
2018-09-24 10:07:25 +0700

    fix error

--- router/routerTestAPI.js
+++ router/routerTestAPI.js
@@ -3,22 +3,22 @@ const log = require("../libs/writeLogs").Logger
 
 module.exports = function (app) {
     app.route('/osource-facebook-webhook')
-        .get((res, resp) => {
-            log.debug("[GET] /osource-facebook-webhook", JSON.stringify(res.body))
+        .get((req, resp) => {
+            log.debug("[GET] /osource-facebook-webhook", JSON.stringify(req.body))
             let VERIFY_TOKEN = "tranminhthang-sccc" // Mã xác minh khi đăng ký webhook Facebook
             let mode = req.query['hub.mode'];
             let token = req.query['hub.verify_token'];
             let challenge = req.query['hub.challenge'];
             if (mode && token) {
                 if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-                    res.status(200).send(challenge);
+                    resp.status(200).send(challenge);
                 } else {
-                    res.sendStatus(403);
+                    resp.sendStatus(403);
                 }
             }
         })
-        .post((res, resp) => {
-            log.debug("[POST] /osource-facebook-webhook", JSON.stringify(res.body))
+        .post((req, resp) => {
+            log.debug("[POST] /osource-facebook-webhook", JSON.stringify(req.body))
             resp.end()
         })
 }
