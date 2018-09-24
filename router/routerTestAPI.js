51b53beb1d5f5b059b144fd0da824ecb84c44ddd
thangse <thangtran.se@gmail.com>
2018-09-24 10:11:44 +0700

    Fix code

--- router/routerTestAPI.js
+++ router/routerTestAPI.js
@@ -4,7 +4,7 @@ const log = require("../libs/writeLogs").Logger
 module.exports = function (app) {
     app.route('/osource-facebook-webhook')
         .get((req, resp) => {
-            log.debug("[GET] /osource-facebook-webhook", JSON.stringify(req))
+            log.debug("[GET] /osource-facebook-webhook", req)
             let VERIFY_TOKEN = "tranminhthang-sccc" // Mã xác minh khi đăng ký webhook Facebook
             let mode = req.query['hub.mode'];
             let token = req.query['hub.verify_token'];
@@ -18,7 +18,7 @@ module.exports = function (app) {
             }
         })
         .post((req, resp) => {
-            log.debug("[POST] /osource-facebook-webhook", JSON.stringify(req))
+            log.debug("[POST] /osource-facebook-webhook", req)
             resp.end()
         })
 }
