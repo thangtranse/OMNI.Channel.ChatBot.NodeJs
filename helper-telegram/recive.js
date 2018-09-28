0fd0e0778b8f2331a111ffa63dd30a59e8bb4992
thangse <thangtran.se@gmail.com>
2018-09-28 16:01:54 +0700

    Telegram fix code

--- helper-telegram/recive.js
+++ helper-telegram/recive.js
@@ -5,16 +5,13 @@ const apiRocket = require("../helper-rocket/apiRest"),
     forwardRocket = require("../libs/forwardRocket")
 
 const handleMessage = async (_data) => {
-    /**
-     * Chưa xử lý trường hợp seen, delivery
-     */
-    if (_data.type != 'message') return;
-    var checkDataUser = await mongodb.findOne(process.env.MONGODB_COLLECTION, { "uid": _data.result.chat.id }).then(data => data).catch(data => data);
+ 
+    var checkDataUser = await mongodb.findOne(process.env.MONGODB_COLLECTION, { "uid": _data.message.chat.id }).then(data => data).catch(data => data);
     var idRoomRocket = null;
     if (checkDataUser) {
         idRoomRocket = checkDataUser.idRoomRocket;
     } else { // chưa có nè
-        let nameSender = _data.result.chat.first_name + "." + _data.result.chat.last_name;
+        let nameSender = _data.message.chat.first_name + "." + _data.message.chat.last_name;
         nameSender = nameSender.toLowerCase().trim().replace(/(\s)/g, ".");
         nameSender = ProcessStr.clearUnikey(nameSender);
         let nameRoomRocket = `Telegram.${nameSender}`;
@@ -27,12 +24,12 @@ const handleMessage = async (_data) => {
             await apiRocket.createOutGoingWebhookRocket(process.env.URL_WEBHOOK_TELEGRAM, nameRoomRocket).then(data => data).catch(data => data);
             idRoomRocket = createRoomRocket.success ? createRoomRocket.channel._id : undefined;
         }
-        let msgRocket = new msgRocketModel.msgRocket("Skype", idRoomRocket, nameRoomRocket, _data.result.chat.id, _data);
+        let msgRocket = new msgRocketModel.msgRocket("Skype", idRoomRocket, nameRoomRocket, _data.message.chat.id, _data);
         mongodb.insert(process.env.MONGODB_COLLECTION, msgRocket.toJson()).then(data => data);
     }
     // kiểm tra giá trị
     if (typeof idRoomRocket == "undefined") return;
-    forwardRocket.forwardRocket(idRoomRocket, _data.result.text, _data.result.chat.first_name + " " + _data.result.chat.last_name, "");
+    forwardRocket.forwardRocket(idRoomRocket, _data.message.text, _data.message.chat.first_name + " " + _data.message.chat.last_name, "");
 }
 
 module.exports = { handleMessage }
--- helper-telegram/send.js
+++ helper-telegram/send.js
@@ -5,7 +5,7 @@ const forwardSkype = async (_data) => {
     var getDataUser = await mongodb.findOne(process.env.MONGODB_COLLECTION, { "idRoomRocket": _data.channel_id }).then(data => data).catch(data => data);
     if (getDataUser && _data.user_name.trim() != process.env.ROCKET_USERNAME) {
         getDataUser.userDetail.text = _data.text;
-        await apiTelegram.sendMessenger(_data.text, getDataUser.userDetail.result.chat.id);
+        await apiTelegram.sendMessenger(_data.text, getDataUser.userDetail.message.chat.id);
     }
 }
 
