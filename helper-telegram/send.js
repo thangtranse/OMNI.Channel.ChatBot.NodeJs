d045b15f57e79e5edf5439cc4fe18932e9d84949
nguyen trong chuot <nguyentrongchuot@nguyens-Air.net.fpt>
2018-09-29 11:42:40 +0700

    fix code

--- helper-telegram/send.js
+++ helper-telegram/send.js
@@ -4,7 +4,6 @@ const mongodb = require('../database/mongodb'),
 const forwardSkype = async (_data) => {
     var getDataUser = await mongodb.findOne(process.env.MONGODB_COLLECTION, { "idRoomRocket": _data.channel_id }).then(data => data).catch(data => data);
     if (getDataUser && _data.user_name.trim() != process.env.ROCKET_USERNAME) {
-        getDataUser.userDetail.text = _data.text;
         await apiTelegram.sendMessenger(_data.text, getDataUser.userDetail.message.chat.id);
     }
 }
