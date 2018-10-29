9f63ce890a9aba38d7607a6c682fad4aa750f9b6
Thangse <mt.tranthang@gmail.com>
2018-10-29 23:52:30 +0700

    Update helper-telegram/send.js

--- helper-telegram/send.js
+++ helper-telegram/send.js
@@ -5,6 +5,7 @@ const forwardSkype = async (_data) => {
     console.log("telegram: ", _data)
     var getDataUser = await mongodb.findOne(process.env.MONGODB_COLLECTION, { "idRoomRocket": _data.channel_id }).then(data => data).catch(data => data);
     if (getDataUser && _data.user_name.trim() != process.env.ROCKET_USERNAME) {
+        console.log("telegram getDataUser: ", getDataUser)
         await apiTelegram.sendMessenger(_data.text, getDataUser.userDetail.message.chat.id);
     }
 }
