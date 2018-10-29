96c2ba6df8f685a770287e3e2a94fa0fba4a58dc
Thangse <mt.tranthang@gmail.com>
2018-10-29 22:29:07 +0700

    Update helper-skype/send.js

--- helper-skype/send.js
+++ helper-skype/send.js
@@ -2,6 +2,7 @@ const mongodb = require('../database/mongodb'),
     apiSkype = require("./apiSkype")
 
 const forwardSkype = async (_data) => {
+    console.log("thangtm", _data);
     var getDataUser = await mongodb.findOne(process.env.MONGODB_COLLECTION, { "idRoomRocket": _data.channel_id }).then(data => data).catch(data => data);
     if (getDataUser && _data.user_name.trim() != process.env.ROCKET_USERNAME) {
         getDataUser.userDetail.text = _data.text;
