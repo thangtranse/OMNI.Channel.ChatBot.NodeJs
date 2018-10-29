32cb1b31cb141e71a17a68b34ec9c96a9f214554
Thangse <mt.tranthang@gmail.com>
2018-10-30 00:04:46 +0700

    t

--- helper-telegram/send.js
+++ helper-telegram/send.js
@@ -6,7 +6,6 @@ const forwardSkype = async (_data) => {
     var getDataUser = await mongodb.findOne(process.env.MONGODB_COLLECTION, { "idRoomRocket": _data.channel_id }).then(data => data).catch(data => data);
     console.log("telegram getDataUser: ", getDataUser)
     if (getDataUser && _data.user_name.trim() != process.env.ROCKET_USERNAME) {
-        
         await apiTelegram.sendMessenger(_data.text, getDataUser.userDetail.message.chat.id);
     }
 }
--- helper-viber/send.js
+++ helper-viber/send.js
@@ -2,7 +2,10 @@ const mongodb = require('../database/mongodb'),
     apiViber = require("./apiViber")
 
 const forwardViber = async (_data) => {
+    console.log("viber show msg", _data)
     var getDataUser = await mongodb.findOne(process.env.MONGODB_COLLECTION, { "idRoomRocket": _data.channel_id }).then(data => data);
+    
+    console.log("viber show getDataUser", getDataUser)
     if (getDataUser && _data.user_name.trim() != process.env.ROCKET_USERNAME) {
         let uidViber = getDataUser.uid;
         apiViber.sendMsg(uidViber, _data.text);
